67e60122f23537e0581d64a029abdf8ecd74fddf
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-07-20 18:00:00 +0430

    prebuilt: Update for twelve
    * Good bye Kiwi and hello Jellyfish
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I2ec148b7723b9733db67db50c14d1f80ddb15f75

---
+++ apps/Android.mk
@@ -0,0 +1,31 @@
+# Copyright (C) 2020 The LibreMobileOS Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Jellyfish
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := jellyfish/Jellyfish.apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := MusicPlayerGO
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := musicplayergo/MusicPlayerGO.apk
+include $(BUILD_PREBUILT)
