436130139976717ab8cc3eb493f6134cd9934ee2
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2024-01-28 17:42:55 +0000

    lmodroid: Welcome Gramophone!
    Change-Id: I452c8eec2ef84e7d53db3f0bfe25f739c8b721e7

--- apps/Android.mk
+++ apps/Android.mk
@@ -16,21 +16,21 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := Jellyfish
+LOCAL_MODULE := Gramophone
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := jellyfish/Jellyfish.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := gramophone/Gramophone.apk
 LOCAL_PRODUCT_MODULE := true
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := MusicPlayerGO
+LOCAL_MODULE := Jellyfish
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := musicplayergo/MusicPlayerGO.apk
+LOCAL_SRC_FILES := jellyfish/Jellyfish.apk
 LOCAL_PRODUCT_MODULE := true
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 include $(BUILD_PREBUILT)
