2a5b99a71cbe6d3b48ad2c7c006fe6504afb6a1a
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2024-01-28 17:43:48 +0000

    lmodroid: RIP VLC
    Change-Id: I988517dd5a1a6e58fc9ae0147f79a690496ec754

--- apps/Android.mk
+++ apps/Android.mk
@@ -36,16 +36,6 @@ LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.side
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := VLC
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := vlc/VLC.apk
-LOCAL_PRODUCT_MODULE := true
-LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := F-Droid
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
