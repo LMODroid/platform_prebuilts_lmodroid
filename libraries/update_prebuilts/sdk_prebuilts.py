ce3b76a9ab00fd73703cb1cb4bd1213a45635327
Dhina17 <dhinalogu@gmail.com>
2024-06-30 00:07:08 +0530

    sdk_prebuilts: Add androidx splashscreen
    Change-Id: I7c250db6e8f4bd5ff713d06cd5b48dce7446e9bd

--- libraries/update_prebuilts/sdk_prebuilts.py
+++ libraries/update_prebuilts/sdk_prebuilts.py
@@ -30,6 +30,7 @@ maven_to_make = {
     'androidx.core:core': { },
     'androidx.core:core-animation': { },
     'androidx.core:core-ktx': { },
+    'androidx.core:core-splashscreen': { },
     'androidx.contentpaging:contentpaging': { },
     'androidx.coordinatorlayout:coordinatorlayout': { },
     'androidx.legacy:legacy-support-core-ui': { },
